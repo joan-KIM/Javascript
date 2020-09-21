c9f457711f3ff15b899476b0c331714658ea5022
joan-KIM <3373minji.kim@gmail.com>
2020-09-21 21:43:57 +0900

    논리연산자 함수 구현 확인

--- javascript/lec01/week01.js
+++ javascript/lec01/week01.js
@@ -1,3 +1,6 @@
-const calcAnd = (a,b) => a*b;
-const calcOr = (a,b) => Boolean(a+b);
-const calcNot = (a,b) => Boolean(a-b);
+// 논리연산자 사용 없이
+export const calcAnd = (a,b) => Boolean(a*b);
+export const calcOr = (a,b) => Boolean(a+b);
+export const calcNot = (a,b) => Boolean(a-b);
+
+// 산술연산자
