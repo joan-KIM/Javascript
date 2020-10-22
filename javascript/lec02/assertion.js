ac0819104a4c96daaf476dbb7189d219d5f42024
joan-KIM <3373minji.kim@gmail.com>
2020-10-23 00:30:21 +0900

    assertion

--- javascript/lec02/assertion.js
+++ javascript/lec02/assertion.js
@@ -1,27 +1,63 @@
 // https://ko.javascript.info/try-catch
 
 export function assert(exception){
-    try {
        if (!exception) {
-        throw new Error("실패함");
+           throw new Error("실패..");
        }
+       return true;
+}
 
-       console.log("문제없음");
-    } catch (error) {
-        console.log(error.name +": " +error.message);
+// export function isEqual(value, equal){
+//     try{
+//         assert(value === equal);
+//         console.log("성공");
+//     }catch(error){
+//         console.log(error.message);
+//     }
+// }
+
+const obj = {
+    a:1,
+    b: {
+      c: 2,
+      d: {
+        e: ['f', { g: 'h' }]
+      },
+      i:[]
     }
+  }
+
+function isObject(obj) {
+    return typeof obj === 'object';
 }
 
-export function expect(value, equal){
-    assert(value == equal);
-}
+export function isEqual(value, equal){
+    if(!(isObject(value) && isObject(equal))){
+        assert(value === equal);
+        return;
+    }
 
-export function expect2(value, equal){
-    if ( value().isArray ){
-        return value().every((element, index) => element === equal[index]);
+    if(Array.isArray(value) && Array.isArray(equal)){
+        assert(value.length === equal.length);
+
+        for (const index in value) {
+            isEqual(value[index], equal[index]);
+        }
+        
     } else{
+        assert(Object.keys(value).length === Object.keys(equal).length);
 
+        for (const property in value) {
+            isEqual(value[property], equal[property]);
+        }
     }
 }
 
-
+export function expect(value, to){
+    try{
+        isEqual(value, to);
+        console.log("성공");
+    }catch(e){
+        console.log(e.message);
+    }
+}
--- javascript/lec02/index.js
+++ javascript/lec02/index.js
@@ -2,6 +2,8 @@ import {customFilter, customEvery, customSome,customForEach,
     customMap,customReduce,customFlat} from './customfunction.js';
 import {datas} from './data.js'
 import {gcd} from '../lec01/gcd.js';
+import {assert, isEqual, expect} from './assertion.js';
+import _ from 'lodash';
 
 Array.prototype.customFilter = customFilter;
 Array.prototype.customEvery = customEvery;
@@ -12,6 +14,15 @@ Array.prototype.customReduce = customReduce;
 Array.prototype.customFlat = customFlat;
 
 export function main(){
+
+    // expect({name : 'minji', birthday : 1220, age:25}, {name : 'minji', birthday : 1220});
+    // expect({name : 'minji', birthday : 1118, height:150}, {name : 'minji', birthday : 1220, height:155})
+
+    // expect( [1,2,3,[4,5]], [1,2,3,[4,5]]);
+    // expect( [1,2,{name : 'minji', birthday : 1220},[4,5]], [1,2,3,[5]]);
+
+    expect({ a:1, b:{c:2, d:{e:['f', {g:'h'}]},i:[]}}, { a:1, b:{c:2, d:{e:['f', {g:'h'}]},i:[]}})
+
     /*
     console.log( [5,10,23,4].customFilter(x => x > 8) );
     console.log( [5,10,23,4].customMap(x => x ** 2) );
@@ -25,6 +36,14 @@ export function main(){
         return acc;
     }, {} ) );
     */
+/*
+    
+    function square(n) {
+        return n * n;
+    }
+       
+    _.map([4, 8], square);
+
 
     const datas2 = datas.customMap( function(movieInfo){
         let newMovieInfo = {};
@@ -38,7 +57,7 @@ export function main(){
         return newMovieInfo;
     } );
 
-    // console.log(datas2);
+    console.log(datas2);
 
     function goodMovie(rating) {
         let newMovies = datas2.customFilter( function(movieInfo){
@@ -47,17 +66,18 @@ export function main(){
         return newMovies;
     }
 
-    // console.log(goodMovie(7));
+    console.log(goodMovie(7));
 
     const datas3 = datas2.customMap( function(movieInfo){
         return movieInfo.genres;
     } );
 
-    // console.log(datas3);
+
+    console.log(datas3);
 
     const datas4 = datas3.flat();
     
-    console.log(datas4);
+    // console.log(datas4);
 
     const datas5 = datas4.customReduce(function(genreSet, genre) {
         if(genreSet.includes(genre)){
@@ -70,4 +90,7 @@ export function main(){
 
     const datas6 = [...new Set(datas4)];
     console.log(datas6);
+
+
+*/
 }
