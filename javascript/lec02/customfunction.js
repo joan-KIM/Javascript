24f3a2f4e7d6074c75c3dea0803adc9b0e584d9e
joan-KIM <3373minji.kim@gmail.com>
2020-11-07 00:04:16 +0900

    customFlat 함수 작성

--- javascript/lec02/customfunction.js
+++ javascript/lec02/customfunction.js
@@ -34,7 +34,7 @@ export function customForEach(callback) {
 }
 
 export function customMap(callback){
-    let arr = new Array(this.length);
+    let arr = new Array(this.length);   // n -> n 이므로 동일한 길이의 배열 객체를 만든다
 
     for(let i = 0; i < this.length; i++){
         arr[i] = callback(this[i],i,this);
@@ -61,18 +61,36 @@ export function customReduce(callback, initialValue) {
 }
 
 export function customFlat(depth) {
+    let arr = this;
+    console.log(arr);
+
+    if (depth === undefined){
+        depth = 1;
+    }else if(depth<1){
+        return [...arr];
+    }
+
+    while (depth) {
+        arr = arr.reduce((acc,val) => {
+           return acc.concat(val);
+        }, []);
+        depth--;
+    }
     
+    return arr;  
 }
 
 function flatDeep(arr, d = 1) {
-    if (d < 1){
-        // return arr.slice();  // 새로운 배열을 만드는 방법 세가지
+    if (d < 1){     // depth가 없거나, 그 이외의 경우
+        // 새로운 배열을 만드는 방법 세가지
+
+        // return arr.slice();   ** arr.slice([begin[, end]]) -> begin이 undefined면 0번 인덱스부터 마지막까지 slice 한다.
         // return [].concat(arr);
         return [...arr];
     }
 
     return arr.reduce((acc, val) => { 
-        if (!Array.isArray(val)) {
+        if (!Array.isArray(val)) { 
             return [...acc, val];
         }
         return acc.concat(flatDeep(val, d - 1));
